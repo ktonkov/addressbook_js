d03ef2e7f3e5e02a3af5af8ca9bc9bb985183f46
Xakaz <xakaz123@gmail.com>
2020-04-01 16:12:39 +0700

    navigation requirements

--- appmanager/NavigationHelper.js
+++ appmanager/NavigationHelper.js
@@ -3,9 +3,17 @@ const { By } = require('selenium-webdriver');
 const { click, type, isElementPresent } = require('./HelperBase');
 
 exports.goToGroupPage = async function (driver) {
-    await click(driver, By.linkText("groups"));
+    var isOnGroupPage = await isElementPresent(driver, By.tagName('h1')) 
+        && await driver.findElement(By.tagName('h1').getText() == 'Groups')
+        && await isElementPresent(driver, By.name('new'));
+    if (!isOnGroupPage) {
+        await click(driver, By.linkText("groups"));
+    }
 }
 
 exports.goToHomePage = async function (driver) {
-    await click(driver, By.linkText("home"));
+    var isOnHomePage = await isElementPresent(driver, By.id('maintable'));
+    if (!isOnHomePage) {
+        await click(driver, By.linkText("home"));
+    }
 }
